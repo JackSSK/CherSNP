f2c9e6e73fe4bd7f68e67332ba69f1d0bb2b57ed
JackK <angrayg@gmail.com>
2020-05-13 03:18:59 -0400

    Update seq_tools.py

--- seq_tools.py
+++ seq_tools.py
@@ -1,3 +1,7 @@
+"""
+General tools
+"""
+
 
 
 def complementary(seq):
@@ -12,3 +16,28 @@ def complementary(seq):
         elif i == 'C':
             ans.append('G')
     return ''.join(ans)
+
+
+def _kmers(alphabet, table, key, k):
+    if (k == 0) :
+        if key not in table:
+            table.append(key)
+            return
+
+    for i in range(4):
+        t = key + alphabet[i]
+        _kmers(alphabet, table, t, k - 1)
+
+def generate(k=1, type='list', pseudo=0):
+    alphabet = ['A', 'C', 'G', 'T']
+    table = []
+    _kmers(alphabet, table, '', k)
+    if type == 'list':return table
+    elif type =='dict':
+        dict = {}
+        for key in table:
+            dict[key] = pseudo
+        return dict
+
+
+print(generate(2,'dict'))
