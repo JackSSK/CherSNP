e07f96c5138a5274f413b84d372427fc30d9e138
JackK <angrayg@gmail.com>
2020-05-16 01:26:01 -0400

    some progress on saving coords

--- trainer.py
+++ trainer.py
@@ -2,6 +2,7 @@ import os
 import re
 import argparse
 from sklearn import svm
+# from itertools import chain
 import read_file as read
 import seq_tools as seqt
 # import feature as fea
@@ -93,22 +94,39 @@ class Trainer:
                 seq = entry.seq[beg-1:end]
                 coord = []
                 cds_contest = []
+                init = []
+                ter = []
+                ent_cds = []
+                ext_cds = []
                 for ele in self.gff[entry.id][trans]['cds']:
                     temp = [int(ele[0])-beg, int(ele[1])-beg]
                     coord.append(temp)
                 if strand == '+':
                     coord = sorted(coord, key=lambda x: x[0])
+                    init.append(coord[0][0])
+                    ter.append(coord[-1][1])
                     for ele in coord:
-                        inseq = seqt.complementary(seq[ele[0]-4:ele[0]+6])
-                        outseq = seqt.complementary(seq[ele[1]-5:ele[1]+5])
+                        enter = ele[0]
+                        exit = ele[1]
+                        if enter != init[0]: ent_cds.append(enter)
+                        if exit != ter[0]: ext_cds.append(exit)
+                        inseq = seqt.complementary(seq[enter-4:enter+6])
+                        outseq = seqt.complementary(seq[exit-5:exit+5])
                         cds_contest.append([inseq, outseq])
                 elif strand == '-':
                     coord = sorted(coord, key=lambda x: -x[0])
+                    init.append(coord[0][1])
+                    ter.append(coord[-1][0])
                     for ele in coord:
-                        outseq = seqt.complementary(seq[ele[0]-4:ele[0]+6])
-                        inseq = seqt.complementary(seq[ele[1]-5:ele[1]+5])
+                        enter = ele[1]
+                        exit = ele[0]
+                        if enter != init[0]: ent_cds.append(enter)
+                        if exit != ter[0]: ext_cds.append(exit)
+                        outseq = seqt.complementary(seq[exit-4:exit+6])
+                        inseq = seqt.complementary(seq[enter-5:enter+5])
                         cds_contest.append([inseq, outseq])
-                print(cds_contest)
+
+                print(coord, cds_contest, init, ent_cds, ext_cds, ter)
         fas_read.close()
 
 
