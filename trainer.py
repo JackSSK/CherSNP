d111aacca131a6d07f7dc786f18056c8197f8998
JackK <angrayg@gmail.com>
2020-08-24 21:26:19 -0400

    Revert "What's other than stop codon.."
    This reverts commit f65e9f4a7753709c2414fa0e3dffd524a5b856b3.

--- trainer.py
+++ trainer.py
@@ -25,9 +25,7 @@ class Trainer:
         self.gff = gffer.Process(gff_file).gff
         # Get observations and correspond dictionary
         self.dict, self.observ = self._observer(seq_file)
-        # Test purpose
-        seqt.encode_json([self.dict, self.observ])
-
+        # seqt.encode_json([self.dict, self.observ])
         # Process in observations and get classifiers
 
     def _observer(self, seq_file):
@@ -130,7 +128,8 @@ class Trainer:
         fas_read.close()
         return dict, observ
 
-    # Things below are to store observations and update correspond dict
+    # This is to store observations of how transcript initiated
+    # Stuffs and Start Codon
     # Now wrong cases are gained by shift 1bp,
     # Maybe shift 3bp instead of 1?
     def _update_init(self, dict, observ, contest, prefix=True):
